ef360619b2f81b4128ceba043f8cd9e8fcc7a950
Roman Fedorenko <romanzhashkow@gmail.com>
2021-11-07 21:13:39 +0200

    Added main

---
+++ src/main/java/HelloWorldDockerized.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class HelloWorldDockerized {
+    public static void main(String[] args) {
+        System.out.printf("Hello World, current date:%s%n", new Date());
+    }
+}
